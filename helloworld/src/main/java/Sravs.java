37f7b9261eed916df9e3da7373c1f853e84ed163
nisha <devimaneesha234@gmail.com>
2021-12-02 10:33:15 +0530

    First change

---
+++ helloworld/src/main/java/Sravs.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Sravs 
+{
+    public static void main(String args[])
+    {   
+    int i;
+	for (i=1;i<=10;i++)
+	{      
+		System.out.println (i);
+	}
+        System.out.println ("\n");
+    }   
+}
